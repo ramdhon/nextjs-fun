16bc32d255c7d7ac9e931bfbd04f31377c3571f4
Dhoni <ramadhoni@keyreply.com>
2022-02-17 23:19:23 +0700

    prepared locales

---
+++ locale/index.js
@@ -0,0 +1,16 @@
+const langs = {
+  en: {
+    navbar: {
+      brand: 'My Next Fun',
+      home: 'Home',
+      todo: 'Todo App',
+      others: 'Other Portofolio'
+    },
+    home: {
+      welcome: 'Resolving inquiries by using tech',
+      follow: 'Catch me up on'
+    }
+  }
+}
+
+export default langs
